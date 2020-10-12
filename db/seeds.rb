e59f25dbd3d125286d19c50ce7f6c81ad37ec858
Micah Nicole Chu Im <micah_chuim@yahoo.com>
2020-10-12 17:08:59 +0800

    User CRUD & Login (#1)
    * created static pages
    
    home, contact and about pages
    
    * edited static pages 
    
    added navbar and layout
    
    * Indention (#6)
    
    * Create user 
    
    user signup functionality
    
    * User login and logout
    
    * user edit and update
    
    * show all users and delete user functionality
    
    * fixed indention
    
    * indention fixed again for all files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Michael Reeves",
+    email: "michael@email.com",
+    password: "password",
+    password_confirmation: "password")
+
+User.create!(name: "Maurice George",
+email: "maurice.george@example.com",
+password: "password",
+password_confirmation: "password")
+
+
+    #rails db:migrate:reset db:seed
+    #rails db:drop
+    #rails db:migrate
+    #rails db:seed
+# More than 1 user
+99.times do |n|
+name = Faker::Food.fruits #=> "Peaches"
+email = "user#{n+1}@email.com"
+password = "password"
+
+User.create!(name: name,
+        email: email,
+        password: password,
+        password_confirmation: password)
+end
